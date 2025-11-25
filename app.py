fba91a6bcfd6d7b3d02a1e1fb173b0e25c67dbc6
HuygnesPython <huygnezindocsgo@gmail.com>
2025-11-25 13:04:32 -0300

    commit

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = f.Flask(__name__)
 
 @app.route('/')
 def index():
-    return f.render_template('index.html')
+    title = 'Página inicial ( Huygnes )'
+    return f.render_template('index.html', title=title)
+
+@app.route('/login')
+def login():
+    title = 'Login'
+    return f.render_template('login.html',title=title)
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True) # <-- REMOVA ISSO no PA!
+    app.run(debug=True) # <-- REMOVA ISSO NO PA!
